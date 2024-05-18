b1b14627f17bac9f0b4e90e6aac6a73703864d80
Liima-M <matheusportinho12@gmail.com>
2024-05-18 07:23:25 -0300

    Aplicando UUIDs nas URI de recursos

--- Rest_API-main/src/main/java/com/matheus/beicinhofoodapi/domain/model/Pedido.java
+++ Rest_API-main/src/main/java/com/matheus/beicinhofoodapi/domain/model/Pedido.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -21,6 +22,8 @@ public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String codigo;
 
     private BigDecimal subtotal;
     private BigDecimal taxaFrete;
@@ -80,10 +83,15 @@ public class Pedido {
 
     private void setStatus(StatusPedido novoStatus){
         if (getStatus().naoPodeAlterarPara(novoStatus)){
-            throw new NegocioException(String.format("Status do pedido %d não pode ser alterado de %S para %s",
-                    getId(), getStatus().getDescricao(), novoStatus.getDescricao()));
+            throw new NegocioException(String.format("Status do pedido %s não pode ser alterado de %S para %s",
+                    getCodigo(), getStatus().getDescricao(), novoStatus.getDescricao()));
         }
         this.status = novoStatus;
+    }
+
+    @PrePersist
+    private void gerarCodigo(){
+        setCodigo(UUID.randomUUID().toString());
     }
 
 }
