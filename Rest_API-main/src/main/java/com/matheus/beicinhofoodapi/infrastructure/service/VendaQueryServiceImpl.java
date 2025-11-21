954d365c9e35ea0940e14bfb217a59189b0c4deb
Liima-M <matheusportinho12@gmail.com>
2025-11-21 11:28:26 -0300

    tratando offset das vendas diararias

--- Rest_API-main/src/main/java/com/matheus/beicinhofoodapi/infrastructure/service/VendaQueryServiceImpl.java
+++ Rest_API-main/src/main/java/com/matheus/beicinhofoodapi/infrastructure/service/VendaQueryServiceImpl.java
@@ -2,14 +2,18 @@ package com.matheus.beicinhofoodapi.infrastructure.service;
 
 import com.matheus.beicinhofoodapi.domain.filter.VendaDiariaFilter;
 import com.matheus.beicinhofoodapi.domain.model.Pedido;
+import com.matheus.beicinhofoodapi.domain.model.StatusPedido;
 import com.matheus.beicinhofoodapi.domain.model.dto.VendaDiaria;
 import com.matheus.beicinhofoodapi.domain.service.VendaQueryService;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 
 @Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
@@ -18,22 +22,41 @@ public class VendaQueryServiceImpl implements VendaQueryService {
     private EntityManager manager;
 
     @Override
-    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
         var builder = manager.getCriteriaBuilder();
+
+        var predicates = new ArrayList<Predicate>();
 
         var query = builder.createQuery(VendaDiaria.class);
         var root = query.from(Pedido.class);
 
+        var functionConvertTzDataCriacao = builder.function("convert_tz", Date.class, root.get("dataCriacao"),builder.literal("+00:00"), builder.literal(timeOffset));
+
         var functionDateDataCriacao = builder.function(
-                "date", Date.class, root.get("dataCriacao"));
+                "date", Date.class, functionConvertTzDataCriacao);
 
         var selection = builder.construct(VendaDiaria.class,
                 functionDateDataCriacao,
                 builder.count(root.get("id")),
                 builder.sum(root.get("valorTotal")));
 
+        if (filtro.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"),
+                    filtro.getRestauranteId()));
+        }
+        if(filtro.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"),
+                    filtro.getDataCriacaoInicio()));
+        }
+        if(filtro.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"),
+                    filtro.getDataCriacaoFim()));
+        }
+        predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+
         query.select(selection);
         query.groupBy(functionDateDataCriacao);
+        query.where(predicates.toArray(new Predicate[0]));
 
         return manager.createQuery(query).getResultList();
     }
